b0b97a09e4e900f996c895789decf009442f0f54
Azondr <210147@htlw10.at>
2025-02-24 13:30:51 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+
+// put function declarations here:
+
+
+void setup() {
+  
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  
+  Serial.println("Hello World");
+  
+  delay(1000);
+
+}
+
+
+// put function definitions here:
+
